07f3b37c611afd519387473a6c1fa5f7aa4d6c66
Rahul <rahulrjz600@gmail.com>
2023-02-23 04:05:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Home from "./Home";
 import {Header} from "./Header";
+import Front from "./Front";
 function App() {
   return (
     <Router>
       <div className="app">
         <Routes>
 
-          <Route path='/checkout' element ={<Header />} />
-          <Route path='/login' element ={<login />} />
-          <Route path='/' element = {<Header />  } />
-          <Route path='/' element = { <Home />} />
+          {/* <Route path='/checkout' element ={<Header />} />
+          <Route path='/login' element ={<login />} /> */}
+          <Route path='/' element = {<Front />  } />
+          
             
         </Routes>
       </div>
